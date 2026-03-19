4390e7e0f17f528ab47a0556a0524f7e55218743
aarontjones <aarontjones4722@gmail.com>
2026-03-19 15:49:33 +0000

    Changing to typescript

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django_distill import distill_path
-from . import views
-
-urlpatterns = [
-    distill_path('', views.home, name='home'),
-    distill_path('aftercare/', views.aftercare, name='aftercare'),
-    distill_path('booking/', views.booking, name='booking'),
-    distill_path('gallery/', views.gallery, name='gallery')
-]
