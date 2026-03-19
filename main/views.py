70638b89df932f91caa8e1ed02762f8716270403
aarontjones <aarontjones4722@gmail.com>
2026-03-19 14:31:47 +0000

    added barebones navbar to each page needed

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Home / Gallery
+def home(request):
+    return render(request, "home.html")
+
+def aftercare(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
