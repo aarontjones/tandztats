455b4ee39ebf18976193049180419c52133574e9
aarontjones <aarontjones4722@gmail.com>
2026-05-20 14:12:56 +0100

    updated gallery, aftercare and biography information

--- dist/main.js
+++ dist/main.js
@@ -156,11 +156,11 @@ function homePage() {
         const aboutText = document.createElement("p");
         aboutText.className = "about-text";
         aboutText.innerText = `
-    I am Harry Tandy, a Bristol and Exeter based tattoo artist.
-
-    I specialise in ________
-
-    I began tattoing ________
+    Bristol and Exeter based tattoo artist, I have currently been tattooing for a year and a half.
+    
+    Pencil and pen-drawn flash, with new designs coming out constantly and lots to choose from already. Customs are also very welcome. Message me on Instagram by clicking on the Instagram icon below.
+    
+    I specialise in single-needle tattoos with either a 3rl or a 1rl, all is done with black ink. My designs tend to be realism/ micro-realism. However, I also do small stick and poke designs, All of which can be seen in both the featured work and the gallery section of this website.
     `;
         aboutContainer.appendChild(aboutText);
         // Right Side - Image
@@ -187,8 +187,8 @@ function homePage() {
         return homeWrapper;
     });
 }
-function createBlogEntry(id, date, blurb, imageFilenames, description) {
-    return { id, date, blurb, imageFilenames, description };
+function createBlogEntry(id, date, blurb, imageFilenames, overallDescription, description) {
+    return { id, date, blurb, imageFilenames, overallDescription, description };
 }
 function parseBlogDate(dateStr) {
     const [day, month, year] = dateStr.split("/").map(Number);
@@ -203,11 +203,21 @@ function buildBlogCard(entry) {
     const blurb = document.createElement("span");
     blurb.className = "blog-entry-blurb";
     blurb.innerText = entry.blurb;
+    const description = document.createElement("div");
+    description.className = "blog-entry-desc";
+    const descText = document.createElement("p");
+    descText.className = "blog-entry-desc-text";
+    descText.innerText = entry.overallDescription;
+    description.appendChild(descText);
     const date = document.createElement("span");
     date.className = "blog-entry-date";
     date.innerText = entry.date;
-    header.appendChild(blurb);
-    header.appendChild(date);
+    const headerTop = document.createElement("div");
+    headerTop.className = "blog-entry-header-top";
+    headerTop.appendChild(blurb);
+    headerTop.appendChild(date);
+    header.appendChild(headerTop);
+    header.appendChild(description);
     // Image grid
     const imageGrid = document.createElement("div");
     imageGrid.className = "blog-entry-images";
@@ -227,12 +237,13 @@ function buildBlogCard(entry) {
         imageGrid.appendChild(img);
     });
     card.appendChild(header);
+    card.appendChild(description);
     card.appendChild(imageGrid);
     return card;
 }
 const blogEntries = [
-    // Format - ID, Date, Blurb, Image(s)
-    createBlogEntry(1, "19/5/2026", "Hand & Machine Poke Designs", ["1000003029.jpg", "1000003030.jpg"], ["Experimenting with hand poke and machine poke designs - These smaller designs are to be used ", "Experimenting with hand poke and machine poke designs."])
+    // Format - ID, Date, Blurb, Image(s), Description, Description of images in Array
+    createBlogEntry(1, "19/5/2026", "Hand & Machine Poke Designs", ["1000003029.jpg", "1000003030.jpg"], "For this flash I used an automated pen made from one of my tattoo machines and a regular non-motorised pen for the smaller pieces you see.", ["The smaller designs will vary; some will be done with a machine, depending on whether they are blacket out or require whip shading. Some will be hand-poked, such as the horse and bull, to name some examples.", "These bigger designs will all be done using a machine."])
 ];
 // Gallery
 function galleryPage() {
@@ -243,7 +254,7 @@ function galleryPage() {
     galleryTitle.innerText = "Gallery";
     const subtext = document.createElement("p");
     subtext.className = "subtext";
-    subtext.innerText = "Here is a collection of my flashes. Tap on any of them to get a closer look, and press CNTRL on your keyboard to zoom in.";
+    subtext.innerText = "Here is a collection of my flashes. Tap on any of them to get a closer look, and press CNTRL on your keyboard to zoom in. If you like any of these, reach out to me on Instagram to enquire and book in!";
     galleryContainer.appendChild(galleryTitle);
     galleryContainer.appendChild(subtext);
     // sort newest first
@@ -263,48 +274,81 @@ function aftercarePage() {
     aftercareTitle.className = "page-title";
     aftercareTitle.innerText = "Aftercare";
     // Aftercare Information
-    // Intro
-    const aftercareIntro = document.createElement("p");
-    aftercareIntro.className = "aftercare-para";
-    aftercareIntro.innerText = "After our session, there are a few rules you will have to follow, to ensure that your fresh tattoo heals in properly.";
-    // 1
-    const aftercareOne = document.createElement("h2");
-    aftercareOne.className = "aftercare-separate";
-    aftercareOne.innerText = "One";
-    // First Para
-    const aftercareParaOne = document.createElement("p");
-    aftercareParaOne.className = "aftercare-para";
-    aftercareParaOne.innerText = `
-    Take off the second skin 2 days after our session, and wash gently with warm water and unscented soap. 
-    
-    If you use a Loofah in the shower, be careful not to rub on the area.`;
-    // 2
-    const aftercareTwo = document.createElement("h2");
-    aftercareTwo.className = "aftercare-separate";
-    aftercareTwo.innerText = "Two";
-    // Second para
-    const aftercareParaTwo = document.createElement("p");
-    aftercareParaTwo.className = "aftercare-para";
-    aftercareParaTwo.innerText = `
-    Moisturise the area twice a day for 1 week, after the second skin is removed: Once in the morning and once in the evening. 
+    const parasOneTitle = document.createElement("h2");
+    parasOneTitle.className = "aftercare-separate";
+    parasOneTitle.innerText = "No Second Skin? No Problem";
+    const parasOne = document.createElement("p");
+    parasOne.className = "aftercare-para";
+    parasOne.innerText = `
+    If you are allergic to adhesives, then Second Skin will not be used.
 
-    I recommend using Natural Cocoa Butter. If the tattoo gets itchy at all - Do not itch, or else some of the ink will peel off. 
+    If Second Skin is not used, the process of healing the tattoo is as follows: The tattoo will be wrapped in cling film with some medical tape to hold it in place post-tattoo. Leave the tattoo under the cling film for about 2-3 hours, then wash the tattoo in warm water and use non-scented soap to properly clean it.
+   
+    Over the course of the next few days, the tattoo will begin to flake, scab, itch and peel. It is best not to pull or itch any of these flaky bits of the tattoo, as this will adversely affect how the tattoo heals. Be especially careful when rubbing the tattoo with a towel or loofah, instead dab it with a paper towel when out of the shower. Avoid submerging it in water for 2 to 4 weeks. Especially avoid salt water and water with chlorine in it, as this will hinder the healing process.
     
-    Same as before, do not rub too harshly with a Loofah or any other shower brush.`;
-    // 3
-    const aftercareThree = document.createElement("h2");
-    aftercareThree.className = "aftercare-separate";
-    aftercareThree.innerText = "Three";
-    // Third para
-    const aftercareParaThree = document.createElement("p");
-    aftercareParaThree.className = "aftercare-para";
-    aftercareParaThree.innerText = `
-    After the week of moisturising, be sure to protect the tattoo from the sun with either sun cream, or covering it up. Do this for at least a month.
+    Lying on a new tattoo can be involuntary, but it is best to avoid pressure on new tattoos so that they may heal more quickly. If the tattoo feels particularly raw, I have always found wrapping it in a layer of clingfilm before bed helps with it rubbing on blankets and sheets. It is also annoying to wash out of clean bed sheets!
+    
+    It is best to also follow the same 4-week rule of thumb for keeping tattoos out of the sun, which can occasionally be a monumental task. Ensure that you stay on top of suncream in very sunny conditions, as this will protect the new tattoo.
 
-    In due time, I will ask for pictures or checkups for how the tattoo is healing. If bits are fading, or the tattoo has completely failed to heal in, I can provide free touch-up sessions
+    For the first week of having the tattoo, you should be moisturising it in the morning and the evening. There is a range of products that can be used. I personally find Palmer's Cocoa Butter is the best for me. 
     
-    If needed, there is a downloadable version of this page below.
+    There are also products such as Bepanthen and hustle butter that achieve the same goal.
     `;
+    // Second paragraph
+    const parasTwoTitle = document.createElement("h2");
+    parasTwoTitle.className = "aftercare-separate";
+    parasTwoTitle.innerText = "Second skin for the win";
+    const parasTwo = document.createElement("p");
+    parasTwo.className = "aftercare-para";
+    parasTwo.innerText = `
+    If second skin is applied, the process is, for the most part, very similar. Second Skin can stay on for 2 to 3 days. Another bit of Second Skin will be given for the tattoo if the previous Second Skin has peeled off the main body of the tattoo. It is also important to monitor air or water bubbles under the Second Skin. Water in the form of sweat, being pressed against the tattoo for a long period of time, can result in infection or poor healing. Therefore, it is important to watch for this and change the layer within the 2 to 3 day period.
+    
+    Second Skin allows you to go about everyday activities; you can shower relatively normally, and it is protected against slight rubbing. However, it is best to keep a level of safety regarding the new tattoo.
+    
+    To peel the Second Skin of, run it under warm water and peel slowly. Stretch the adhesive parallel to the skin; this way it will be considerably more painless than tearing it off while dry. While in the shower, you should wash the tattoo gently with unscented soap. Then begin the week-long moisturising process, morning and evening, with any of the products detailed above.
+    
+    Be additionally aware of the dangers detailed above. Submerging the new tattoo in water, patting it dry when out of the shower, avoiding putting pressure on the tattoo and keeping it out of the sun.
+    
+    This is understandably a lot of information. However, following these steps should hopefully lead to the best result for your new tattoo!
+    `;
+    // Links to amazon page
+    const productsTitle = document.createElement("h2");
+    productsTitle.className = "aftercare-products-title";
+    productsTitle.innerText = "Recommended Products";
+    const productsRow = document.createElement("div");
+    productsRow.className = "aftercare-products";
+    const products = [
+        { label: "Cocoa Butter", url: "https://www.amazon.co.uk/Palmers-Formula-Intensive-Moisturizing-Hydration/dp/B0FK6KPSCK/ref=sr_1_4_sspa?crid=395RLG9JT3LC0&dib=eyJ2IjoiMSJ9.0maMJVlYvmRzHbUfic6NiW9QVVS51HM31cry6EZrg9p5H9E9Eq0dgrVP5K_77yAuR9XpdPjlwiCQzaBqOhi6PW_KgjSipetLlaI_B55sNw6_miSPwOnTDlJARbrmd5Tm2LUb9BzsJL6H913QnUQbm9H6QDjg_Vv1dqZzOMR9Um7LVGSkRb9uXo7MNltCfOIkqSoeU1PxWaqnSDRz5MWunnngUE8pr4nAMbatdhS9my7BY8sUuvzdCOOBCnuQzZMk1rY0b8YEQYQEadnQk-qu7aBYhMnIPKlE5iOoP1iW1T0.MkIWqGhQwBxbdXxvv4kenDTc7033ozE2Fi0vFWXeC0w&dib_tag=se&keywords=cocoa+butter&qid=1779272001&sprefix=cocobutter%2Caps%2C139&sr=8-4-spons&aref=dh92qsiDNK&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1" },
+        { label: "Bepanthen", url: "https://www.amazon.co.uk/Bepanthen-Tattoo-Intense-Ointment-Provitamin/dp/B08MW387NQ/ref=asc_df_B08MW387NQ?mcid=6e210095f96431749bf3e3c0aa0a9ade&th=1&tag=googshopuk-21&linkCode=df0&hvadid=697192275042&hvpos=&hvnetw=g&hvrand=18030686021561345850&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9045616&hvtargid=pla-1125883823896&hvocijid=18030686021561345850-B08MW387NQ-&hvexpln=0" },
+        { label: "Hustle Butter", url: "https://www.amazon.co.uk/Hustle-Butter-Organic-Tattoo-Bulldog/dp/B0B6JMQZZ3/ref=asc_df_B0B6JMQZZ3?mcid=6adc1f7760c43856b05a330159710f10&tag=googshopuk-21&linkCode=df0&hvadid=697261239304&hvpos=&hvnetw=g&hvrand=15933544647302166084&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9045616&hvtargid=pla-1720329433385&psc=1&hvocijid=15933544647302166084-B0B6JMQZZ3-&hvexpln=0" }
+    ];
+    products.forEach(({ label, url }) => {
+        const link = document.createElement("a");
+        link.href = url;
+        link.target = "_blank";
+        link.className = "aftercare-product-btn";
+        const icon = document.createElement("img");
+        icon.src = "./assets/icons/amazon.svg";
+        icon.className = "aftercare-product-icon";
+        const text = document.createElement("span");
+        text.innerText = label;
+        link.appendChild(icon);
+        link.appendChild(text);
+        productsRow.appendChild(link);
+    });
+    // Aftercare Columns
+    const aftercareCols = document.createElement("div");
+    aftercareCols.className = "aftercare-cols";
+    const colOne = document.createElement("div");
+    colOne.className = "aftercare-col";
+    const colTwo = document.createElement("div");
+    colTwo.className = "aftercare-col";
+    colOne.appendChild(parasOneTitle);
+    colOne.appendChild(parasOne);
+    colTwo.appendChild(parasTwoTitle);
+    colTwo.appendChild(parasTwo);
+    aftercareCols.appendChild(colOne);
+    aftercareCols.appendChild(colTwo);
     // Printing Page
     const printPage = document.createElement("a");
     printPage.href = "#";
@@ -319,14 +363,9 @@ function aftercarePage() {
         window.print();
     });
     aftercareContainer.appendChild(aftercareTitle);
-    // Appending all my stupid aftercare info bits
-    aftercareContainer.appendChild(aftercareIntro);
-    aftercareContainer.appendChild(aftercareOne);
-    aftercareContainer.appendChild(aftercareParaOne);
-    aftercareContainer.appendChild(aftercareTwo);
-    aftercareContainer.appendChild(aftercareParaTwo);
-    aftercareContainer.appendChild(aftercareThree);
-    aftercareContainer.appendChild(aftercareParaThree);
+    aftercareContainer.appendChild(aftercareCols);
+    aftercareContainer.appendChild(productsTitle);
+    aftercareContainer.appendChild(productsRow);
     aftercareContainer.appendChild(printPage); // Print Symbol
     return aftercareContainer;
 }
